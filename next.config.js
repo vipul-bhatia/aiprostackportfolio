f20e1ff436bc77061b540b067822f11331dc210c
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-09-09 11:39:22 +0530

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        domains: ['aiprostacksolutions-portfolio.onrender.com'],
+    },
 }
 
 module.exports = nextConfig
