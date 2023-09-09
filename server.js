9aba9bad5047980778f2da8894db3fcac5a6269a
vipul-bhatia <vipul0592bhatia@gmail.com>
2023-09-09 11:16:20 +0530

    .

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const { https } = require('firebase-functions');
-const { default: next } = require('next');
+// const { default: next } = require('next');
 
-const isDev = process.env.NODE_ENV !== 'production';
+// const isDev = process.env.NODE_ENV !== 'production';
 
-const server = next({
-    dev: isDev,
-    //location of .next generated after running -> yarn build
-    conf: { distDir: '.next' },
-});
+// const server = next({
+//     dev: isDev,
+//     //location of .next generated after running -> yarn build
+//     conf: { distDir: '.next' },
+// });
 
-const nextjsHandle = server.getRequestHandler();
-exports.nextServer = https.onRequest((req, res) => {
-    return server.prepare()
-        .then(() => {
-            return nextjsHandle(req, res)
-        });
-});
+// const nextjsHandle = server.getRequestHandler();
+// exports.nextServer = https.onRequest((req, res) => {
+//     return server.prepare()
+//         .then(() => {
+//             return nextjsHandle(req, res)
+//         });
+// });
 
-/*
-firebase-admin,firebase-functions
-require these plugins,install them
-*/
+// /*
+// firebase-admin,firebase-functions
+// require these plugins,install them
+// */
